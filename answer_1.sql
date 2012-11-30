4d423989a25179f6323a2f4ba09460109505687e
yifan ma <leoyfm@gmail.com>
2012-11-30 13:41:15 +1100

    Answert 1 ============== test

--- answer_1.sql
+++ answer_1.sql
@@ -1,3 +1,6 @@
+-- -----------------------------------------------------
+-- Answer 1: calculate how many guitars were sold
+-- -----------------------------------------------------
 SELECT SUM(c.quantity)
 FROM cart_items c JOIN product p on c.product_id = p.id
 WHERE p.name = 'Guitar'
